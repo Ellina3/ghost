c90de41e049a6e92fa61b056a3a0e30596141ed0
ellina <novira@tut.by>
2018-11-05 20:48:19 +0300

    modified files

---
+++ init.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	
+	// Находим блок карусели
+	var carousel = $("#carousel");
+
+	// Запускаем плагин карусели
+	carousel.owlCarousel({
+        // Точки под каруселью
+        itemsDesktop: [1600, 1],
+        loop: true,
+        autoPlay: true,
+        autoPlayTimeout: 500,
+        items: 1,
+        scrollPerPage: 1,
+    });
+});
